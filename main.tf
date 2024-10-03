f62091b49f2052443cef517a98b69aae6c9f2d52
sahilsharma2184 <sahilsharma2184@gmail.com>
2024-10-04 00:03:57 +0530

    Infrastructure created using terraform script

---
+++ main.tf
@@ -0,0 +1,23 @@
+provider "aws" {
+    region = "us-east-1" 
+}
+
+resource "aws_instance" "myways_instance" {
+    ami ="ami-0866a3c8686eaeeba"
+    instance_type = "t2.micro"
+    key_name = "ec2_myways_KP"
+    vpc_security_group_ids = ["sg-058d2104aa47f4ad3"]
+}
+
+resource "aws_s3_bucket" "myways_s3_bucket" {
+  bucket = "myways-s3-bucket-2184"
+}
+
+resource "aws_secretsmanager_secret" "api_credentials" {
+  name        = "api_credentials_2184"
+  description = "API credentials for document processing."
+}
+
+resource "aws_sqs_queue" "document_queue" {
+  name = "document-processing-queue_2184"
+}
